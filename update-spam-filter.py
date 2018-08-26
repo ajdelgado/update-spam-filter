06c6b5622b54ddb71b79c721d363aa8f90da3c57
Antonio J. Delgado <antoniodelgado@susurrando.com>
2018-08-26 13:01:36 +0200

    Added check for bytes recipient before decoding

--- update-spam-filter.py
+++ update-spam-filter.py
@@ -185,6 +185,8 @@ def SendWarning(ORIGINALMTA,MSGID,HEADERS):
   else:
     for RECIPIENT in RECIPIENTS:
       if not AlreadyNotified(ORIGINALMTA,RECIPIENT):
+        if type(RECIPIENT) == bytes:
+          RECIPIENT=RECIPIENT.decode('utf-8')
         MESSAGE="Hi,\nThe server %s was added to our spam list because is sending spam messages like the message id %s.\nPlease, check the server and report back in case you would like to remove it from our list.\nYou're receiving this message because you are in the whois record for the domain %s.\nThanks\n\n\nHeaders of the message:%s" % (ORIGINALMTA,MSGID,DOMAIN,HEADERS)
         server = smtplib.SMTP('localhost')
         server.sendmail(SENDER, RECIPIENT.decode('utf-8'), MESSAGE)

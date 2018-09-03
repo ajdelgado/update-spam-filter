ec9cebcc9455f4b864f7f962c8872fe2c2f130a2
antonio <antonio@spotify.com>
2018-09-03 23:07:51 +0200

    joined global vars

--- update-spam-filter.py
+++ update-spam-filter.py
@@ -98,8 +98,7 @@ def IsExcludeMTA(MTA):
 
 
 def ProcessArguments():
-    global DEBUG, IMAPSERVER, IMAPPORT, IMAPUSER, IMAPPASSWORD, IMAPMAILBOX
-    global SSL, IMAPFILTER
+    global DEBUG, IMAPSERVER, IMAPPORT, IMAPUSER, IMAPPASSWORD, IMAPMAILBOX, SSL, IMAPFILTER, CSVOUTPUT
     for arg in sys.argv:
         if arg == "-h" or arg == "--help" or arg == "-?" or arg == "/?" or arg == "/h" or arg == "/help":
             Usage()

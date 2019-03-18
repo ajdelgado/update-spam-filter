7cc1c7bc2adca1bbca1dbdd71e9347ee7e50761c
Antonio J. Delgado <antoniodelgado@susurrando.com>
2019-03-18 22:40:20 +0100

    removed close

--- update-spam-filter.py
+++ update-spam-filter.py
@@ -535,7 +535,6 @@ except imaplib.IMAP4.error as e:
     log.error("Error login as '%s@%s:%s'. %s" %
               (config['imapuser'], config['imapserver'],
                config['imapport'], e))
-    IMAP.close()
     sys.exit(1)
 log.info("Selecting mailbox %s..." % config['imapmailbox'])
 try:

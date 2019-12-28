8b8130408bec723df3849d5a3d7e6877222c9227
Antonio Jesus Delgado Linares <adelgadolina@opentext.com>
2019-12-28 18:43:14 +0200

    added imap test

--- imap_test.py
+++ imap_test.py
@@ -1,13 +1,22 @@
 import imaplib
 from email.parser import HeaderParser
 
-conn = imaplib.IMAP4_SSL('dedi.susurrando.com')
-conn.login('ad@susurrando.com', 'SME35PyD$U5dwiW5K')
+
+def get_original_mta(msg):
+    last_mta = ""
+    for k, v in msg.items():
+        if k == "Received":
+            last_mta = v.split(" ")[1]
+    return last_mta
+
+
+conn = imaplib.IMAP4_SSL("dedi.susurrando.com")
+conn.login("user@example.com", "aP4ssw0rd")
 conn.select()
-status, idata = conn.search(None, 'ALL') # returns a nice list of messages...
-                         # let's say I pick #1 from this
+status, idata = conn.search(None, "ALL")  # returns a nice list of messages...
+# let's say I pick #1 from this
 print(idata)
-status, data = conn.fetch('1', "(FLAGS BODY[HEADER])")
+status, data = conn.fetch("1", "(FLAGS BODY[HEADER])")
 
 # gloss over data structure of return... I assume you know these
 # gives something like:
@@ -16,4 +25,5 @@ header_data = data[0][1].decode("utf-8")
 
 parser = HeaderParser()
 msg = parser.parsestr(header_data)
-print(msg.get('Subject', ''))
+print(msg.keys())
+print(get_original_mta(msg))
